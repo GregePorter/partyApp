1ea4aecb608edd70b1ee0880893a5823c629ddec
Al Sahin <alsahin@me.com>
2014-03-21 11:02:52 -0500

    initial commit for partyApp

---
+++ index.js
@@ -0,0 +1,26 @@
+// Module dependencies. 
+var express = require('express');
+var http = require('http');
+//var site = require('./controllers/site');
+
+//create a new express app
+var app = express();
+
+//Config
+app.configure(function(){
+  	app.set('port', process.env.PORT || 3000);
+  	app.use(express.static(__dirname + '/public')); 
+  	app.set('views', __dirname + '/views');
+  	app.set('view engine', 'jade');
+  	app.use(express.bodyParser()); // ???
+  	app.use(express.logger('dev')); //dev, short, default, tiny
+});
+
+
+//SITE ROUTES
+//app.get('/', site.home);
+
+//create server:port
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
