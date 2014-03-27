68c3b1df0abea4f587f05d3cea9da4e38c619b69
GregePorter <greg.e.porter@gmail.com>
2014-03-27 17:20:13 -0500

    cleaned up files

--- public/js/views/person.js
+++ public/js/views/person.js
@@ -3,11 +3,10 @@ define([
     'jqgrid',
     'underscore',
     'backbone',
-    "handlebars",
     'hbs!../templates/options',
     'hbs!../templates/party',
     'hbs!../templates/birthday'
-], function($, jqGrid, _, Backbone, Handlebars, options_template, party_template, birthday_template){
+], function($, jqGrid, _, Backbone, options_template, party_template, birthday_template){
     var PersonView = Backbone.View.extend({
         tagName: 'ul', //this.el is a list element
         className: 'thumbnail', //add this classname to the list element
@@ -31,20 +30,17 @@ define([
                         $(this.el).html(options_template()); 
                 }
             }
-            //pass the Month model and compile this.template and put generated HTML in this.el 
             $("#date_input").datepicker({changeYear:true, changeMonth:true,yearRange:"1950:2020"});
-            //return context to enable chained calls
             return this;
         },
         
         events: {
             //select my Month of Birth on click
             "click .type" : "render",
-            "change":  "updateRow",
+            "change":  "updateModel",
             "click .save":  "savePerson"
         },
-        //change model with the selected Month of Birth
-        updateRow : function (event) {
+        updateModel : function (event) {
             console.log("update triggered");
             var target = event.target;
             this.model.set(target.name , target.value);
